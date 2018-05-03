e5522332a70552bc559365ebfa92b2de8f13282c
MINSK\Aliaksandr_Dzemidovi <Aliaksandr.Dzemidovich@thomsonreuters.com>
2018-05-03 09:31:18 +0300

    add sql missing files

---
+++ Module6/Task3/Northwind/Northwind_v1.1/Scripts/ScriptsIgnoredOnImport.sql
@@ -0,0 +1,28 @@
+﻿
+-- 3
+
+-- 1.0 -> 1.1
+
+USE Northwind;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects 
+	WHERE OBJECT_ID = OBJECT_ID(N'dbo.CreditCard') AND TYPE IN (N'U'))
+BEGIN
+
+--The following statement was imported into the database project as a schema object and named dbo.CreditCard.
+--CREATE TABLE CreditCard   
+--(  
+--    CreditCardID INT IDENTITY(1,1) NOT NULL, 
+--	CreditCardNumber NVARCHAR(12) NOT NULL,
+--    ExpiryDate DATETIME NOT NULL,   
+--    HolderLastName NVARCHAR(20) NOT NULL,
+--	HolderFirstName NVARCHAR(10) NOT NULL,
+--	HolderID INT NULL
+--	CONSTRAINT FK_CreditCard_Employee FOREIGN KEY(HolderID)
+--		REFERENCES Employees (EmployeeID)
+--);
+  
+
+END;
+GO

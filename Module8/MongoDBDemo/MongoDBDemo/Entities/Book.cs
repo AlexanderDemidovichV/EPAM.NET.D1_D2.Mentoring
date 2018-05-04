1716a793295474df6dfd004ba226f66937d1ef39
MINSK\Aliaksandr_Dzemidovi <Aliaksandr.Dzemidovich@thomsonreuters.com>
2018-05-04 10:42:45 +0300

    add demo module 8, tasks done, need to refactore

---
+++ Module8/MongoDBDemo/MongoDBDemo/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
+
+namespace MongoDBDemo.Entities
+{
+    public class Book
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("author")]
+        public string Author { get; set; }
+
+        [BsonElement("count")]
+        public int Count { get; set; }
+
+        [BsonElement("genre")]
+        public string[] Genre { get; set; }
+
+        [BsonElement("year")]
+        public int Year { get; set; }
+    }
+}
